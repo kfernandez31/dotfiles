0010f6b0f1fcc1c61d88b95ea6aee57105eb41d4
kfernandez31 <kacper.fernandez@proton.me>
2024-08-07 13:43:20 +0200

    Update for 07.08.24

---
+++ extras/scripts/font-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "Normal"
+echo -e "\033[1mBold\033[22m"
+echo -e "\033[3mItalic\033[23m"
+echo -e "\033[3;1mBold Italic\033[0m"
+echo -e "\033[4mUnderline\033[24m"
+echo -e "== === !== >= <= =>"
+echo -e "契          勒 鈴 "
